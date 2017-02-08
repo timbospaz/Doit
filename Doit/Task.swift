3f770f6d1b6d4042ff777c27d2062ee1721c4cea
Timothy Sparrow <timbospaz@gmail.com>
2017-02-08 17:43:23 +0100

    Do it project so far

---
+++ Doit/Task.swift
@@ -0,0 +1,16 @@
+//
+//  Task.swift
+//  Doit
+//
+//  Created by Timothy Sparrow on 05/02/2017.
+//  Copyright © 2017 Timothy Sparrow. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    
+    var name = ""
+    var important = false
+    
+}
